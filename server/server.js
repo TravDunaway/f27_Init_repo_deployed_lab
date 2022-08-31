2fdeba379427d1d64734824945a36720bebdd9b8
Travis <travisndunaway@gmail.com>
2022-08-31 15:15:16 -0600

    intialrepopush

---
+++ server/server.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/index.html")); 
+});
+
+app.get("/css", (req, res) => {
+res.sendFile(path.join(__dirname, "../client/index.html"));
+});
+
+app.get("/js", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/index.html"));
+    });
+
+const port = process.env.PORT || 4005;
+
+app.listen(port, () => {
+    console.log(`listening on port ${port}`);
+});
